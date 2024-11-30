7d698e488b2ab9a49d227ef772792766c82f1b15
dyh <a@adeMacBook-Pro.local>
2024-11-30 11:32:25 +0800

    修改

---
+++ python/sort/bubble_sort.py
@@ -0,0 +1,17 @@
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+    return arr
+
+
+
+if __name__ == '__main__':
+    arr = [4,8,9,6,7,2]
+    res = bubble_sort(arr)
+    print(res)
---
+++ python/sort/insert_sort.py
@@ -0,0 +1,17 @@
+
+def insert_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+if __name__ == '__main__':
+    arr = [3,2,1,7,0,4,5]
+    res = insert_sort(arr)
+
+    print(res)
